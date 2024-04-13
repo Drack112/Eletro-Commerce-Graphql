ca866fed428265a1c50f6a75dc52b12a53f358cd
João Vitor <onepessoa3@gmail.com>
2024-04-13 11:14:07 -0300

    [FEAT] - MServiços e resolvers para buscar categorias e produtos

--- backend/src/modules/product/product.resolver.ts
+++ backend/src/modules/product/product.resolver.ts
@@ -2,6 +2,8 @@ import { Args, Query, Resolver } from '@nestjs/graphql';
 import { ProductService } from './product.service';
 import { PaginatedProduct } from './dto/paginated-products.object-types';
 import { PaginationInput } from 'src/common/dto/pagination.input';
+import { Product } from './schemas/product.schema';
+import { CategoryBrands } from './dto/category-brands.object-type';
 
 @Resolver()
 export class ProductResolver {
@@ -13,5 +15,72 @@ export class ProductResolver {
     pagination?: PaginationInput,
   ) {
     return await this.productService.findMany(pagination);
+  }
+
+  @Query(() => PaginatedProduct)
+  public async productsByBrand(
+    @Args('brand') brand: string,
+    @Args('pagination', { nullable: true }) pagination?: PaginationInput,
+  ) {
+    return await this.productService.findManyByBrand(brand, pagination);
+  }
+
+  @Query(() => PaginatedProduct)
+  public async productsByCategory(
+    @Args('category') category: string,
+    @Args('pagination', { nullable: true }) pagination?: PaginationInput,
+  ) {
+    return await this.productService.findManyByCategory(category, pagination);
+  }
+
+  @Query(() => PaginatedProduct)
+  public async productsOfBrandByCategory(
+    @Args('category') category: string,
+    @Args('brand') brand: string,
+    @Args('pagination', { nullable: true }) pagination?: PaginationInput,
+  ) {
+    return await this.productService.findProductsOfBrandByCategory(
+      category,
+      brand,
+      pagination,
+    );
+  }
+
+  @Query(() => PaginatedProduct)
+  public async queryProducts(
+    @Args('q') q: string,
+    @Args('pagination', { nullable: true }) pagination?: PaginationInput,
+  ) {
+    return await this.productService.queryProducts(q, pagination);
+  }
+
+  @Query(() => [Product])
+  public async topProducts(@Args('limit', { nullable: true }) limit?: number) {
+    return await this.productService.findTopProducts(limit);
+  }
+
+  @Query(() => [Product])
+  public async latestProducts(
+    @Args('limit', { nullable: true }) limit?: number,
+  ) {
+    return await this.productService.findLatestProducts(limit);
+  }
+
+  @Query(() => [String])
+  public async allCategories() {
+    return await this.productService.getCategoryList();
+  }
+
+  @Query(() => [String])
+  public async brandsByCategory(
+    @Args('category')
+    category: string,
+  ) {
+    return await this.productService.brandsByCategory(category);
+  }
+
+  @Query(() => [CategoryBrands])
+  public async categoryBrands() {
+    return await this.productService.getCategoryBrands();
   }
 }
--- backend/src/modules/product/product.service.ts
+++ backend/src/modules/product/product.service.ts
@@ -1,15 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Product } from './schemas/product.schema';
 import { PaginatedProduct } from './dto/paginated-products.object-types';
 import { PaginationInput } from 'src/common/dto/pagination.input';
+import { CategoryBrands } from './dto/category-brands.object-type';
 
 @Injectable()
 export class ProductService {
   constructor(
     @InjectModel(Product.name)
-    private productModeL: Model<Product>,
+    private productModel: Model<Product>,
   ) {}
 
   public async findMany(
@@ -18,13 +19,143 @@ export class ProductService {
     const limit = pagination?.limit || 25;
     const page = pagination?.page || 1;
 
-    const products = await this.productModeL
+    const products = await this.productModel
       .find({})
       .skip((page - 1) * limit)
       .limit(limit)
       .lean();
 
-    const count = await this.productModeL.countDocuments({});
+    const count = await this.productModel.countDocuments({});
     return { count, products };
+  }
+
+  public async findManyByBrand(
+    brand: string,
+    pagination?: PaginationInput,
+  ): Promise<PaginatedProduct> {
+    const limit = pagination?.limit || 25;
+    const page = pagination?.page || 1;
+
+    const products: Product[] = await this.productModel
+      .find({ brand: new RegExp('^' + brand + '$', 'i') })
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .lean();
+    const count = await this.productModel.countDocuments({
+      brand: new RegExp('^' + brand + '$', 'i'),
+    });
+    return { count, products };
+  }
+
+  public async findManyByCategory(
+    category: string,
+    pagination?: PaginationInput,
+  ): Promise<PaginatedProduct> {
+    const limit = pagination?.limit || 25;
+    const page = pagination?.page || 1;
+    const products: Product[] = await this.productModel
+      .find({ category: new RegExp('^' + category + '$', 'i') })
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .lean();
+    const count = await this.productModel.countDocuments({
+      category: new RegExp('^' + category + '$', 'i'),
+    });
+    return { count, products };
+  }
+
+  public async findProductsOfBrandByCategory(
+    category: string,
+    brand: string,
+    pagination?: PaginationInput,
+  ) {
+    const limit = pagination?.limit || 25;
+    const page = pagination?.page || 1;
+    const products: Product[] = await this.productModel
+      .find({
+        category: new RegExp('^' + category + '$', 'i'),
+        brand: new RegExp('^' + brand + '$', 'i'),
+      })
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .lean();
+    const count = await this.productModel.countDocuments({
+      category: new RegExp('^' + category + '$', 'i'),
+      brand: new RegExp('^' + brand + '$', 'i'),
+    });
+    return { count, products };
+  }
+
+  public async queryProducts(
+    q: string,
+    pagination?: PaginationInput,
+  ): Promise<PaginatedProduct> {
+    const limit = pagination?.limit || 25;
+    const page = pagination?.page || 1;
+    const products: Product[] = await this.productModel
+      .find({
+        $text: { $search: `\"${q}\"` },
+      })
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .lean();
+
+    const count = await this.productModel.countDocuments({
+      $text: { $search: `\"${q}\"` },
+    });
+    return { count, products };
+  }
+
+  public async findTopProducts(limit?: number) {
+    const safeLimit = limit | 25;
+    const products: Product[] = await this.productModel
+      .find({})
+      .sort({ rating: -1 })
+      .limit(safeLimit)
+      .lean();
+
+    return products;
+  }
+
+  public async findLatestProducts(limit?: number) {
+    const safeLimit = limit | 25;
+    const products: Product[] = await this.productModel
+      .find({})
+      .sort({ createdAt: -1 })
+      .limit(safeLimit)
+      .lean();
+
+    return products;
+  }
+
+  public async getCategoryList(): Promise<string[]> {
+    const categories = await this.productModel.find().distinct('category');
+    return categories;
+  }
+
+  public async brandsByCategory(category: string): Promise<string[]> {
+    const brands = await this.productModel
+      .find({
+        category: new RegExp('^' + category + '$', 'i'),
+      })
+      .distinct('brand');
+
+    return brands;
+  }
+
+  public async getCategoryBrands(): Promise<CategoryBrands[]> {
+    const result: CategoryBrands[] = [];
+
+    try {
+      const categories = await this.getCategoryList();
+      for (const category of categories) {
+        const brands = await this.brandsByCategory(category);
+        result.push({ category, brands });
+      }
+
+      return result;
+    } catch (error) {
+      throw new HttpException(error.message, 500);
+    }
   }
 }
